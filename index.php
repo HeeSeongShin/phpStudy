ae29e6233d35a2d90d07a83c2c201975f3924282
HeeSeongShin <gmltjd3922@naver.com>
2022-10-03 14:16:11 +0900

    게시판 구조

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-hello world
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width= , initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form method="POST" action="./submit.php">
+    <input type="text" name="id"/>
+    <input type="text" name="pw"/>
+    <button>제출</button>
+  </form>
+</body>
+</html>
---
+++ submit.php
@@ -0,0 +1,8 @@
+<?php
+  function login() {
+    // 로그인 처리
+    header("Location: ./view.php");
+  }
+
+  login();
+?>
